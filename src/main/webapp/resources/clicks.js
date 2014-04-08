08edc16a085e0d6176c16baee62d51f23d20cc09
Natalia Bartol <natalia.b@zend.com>
2014-04-08 01:36:54 +0100

    Better array cleaning

--- src/main/webapp/resources/clicks.js
+++ src/main/webapp/resources/clicks.js
@@ -31,7 +31,6 @@ $(document).ready(function() {
 			url : window.location.pathname + "clicks",
 			type : 'GET',
 			success : function(data) {
-				dps.length = 0;
 				backupPlayerColors = $.extend(true, [], playerColors);
 				playerColors = [];
 				var i = 0;
